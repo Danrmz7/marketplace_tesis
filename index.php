bf99217ea83ccf5c6590b4ca038ba054980127cc
irving <irving.rmz00@gmail.com>
2024-08-08 16:45:17 -0600

    Primer commit. Inicio de proyecto

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- favicon/relicon -->
+    <link rel="shortcut icon" href="_assets/img/favicon.png">
+
+    <!-- css -->
+    <link rel="stylesheet" href="_assets/css/bootstrap.min.css">
+</head>
+<body>
+    <?php include ("_assets/includes/navbar.inc.php") ?>
+    <div class="container">
+        
+    </div>
+
+    <script src="_assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

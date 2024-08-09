cd36fae19b73edebef72c1d82884a18eddbcfd47
irving <irving.rmz00@gmail.com>
2024-08-09 10:18:54 -0600

    login en proceso

--- login.php
+++ login.php
@@ -4,8 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <!-- css -->
+    <link rel="stylesheet" href="_assets/css/bootstrap.min.css">
 </head>
 <body>
+
+    <form method="post" role="form" action="./">
+			<!-- <input type="text" class="form-control form-control-lg input-lg" name="nombre" placeholder="Nombre De Usuario" required> -->
+			<!-- <input type="password" class="form-control form-control-lg input-lg" name="contrasena" placeholder="Contraseña" required> -->
+			<!-- <p style="color:darkred;"><?php echo ($_GET['error']?$_GET['error']:''); ?></p> -->
+			<!-- <input name="remember_me" type="checkbox" value="Remember Me" checked>
+			<label for="remember-me"> Remember Me</label> -->
+        <!-- <button type="submit" class="btn btn-success btn-lg btn-block"><i class="icon-unlock2"></i> Iniciar Sesión</button> -->
+	</form>
     
 </body>
 </html>
